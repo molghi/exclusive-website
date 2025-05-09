20b0baa5cdcf751fb3d42ad5425d8b5d930f7a2e
molghi <54518k@gmail.com>
2025-05-09 04:34:41 +0300

    added github pages auto deploy

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
     <Provider>
-        <BrowserRouter>
+        <BrowserRouter basename="/exclusive-website">
             <React.StrictMode>
                 <App />
             </React.StrictMode>
